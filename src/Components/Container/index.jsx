68a9840c6b44e594fcf374abf3ef3401e547c0e7
João Araújo <joaogabriel-al@hotmail.com>
2022-05-07 18:04:20 -0300

    03 - 05.Media Querie

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -14,13 +14,17 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
 `;
 
 export default () => {
   return (
     <Container>
       <Titulo>Olá Fulano!</Titulo>
-      <Conteudo className="conteudo">
+      <Conteudo>
         <Conta />
       </Conteudo>
     </Container>
